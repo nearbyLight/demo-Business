2ff6b2c589c438a5d89615ba4af723843602be10
xiaoqiang <359242762@qq.com>
2021-08-13 14:04:58 +0800

    每日优鲜-B-系统初始布局

---
+++ src/views/components/main-list.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="main-header">
+        <a-button
+          type="primary"
+          style="margin-bottom: 16px"
+          @click="toggleCollapsed"
+        >
+          <a-icon :type="$store.state.collapsed ? 'menu-unfold' : 'menu-fold'" />
+        </a-button>
+        <!-- 面包屑导航 -->
+        <div class="breakcmurb">
+          <a-breadcrumb>
+            <a-breadcrumb-item>Home</a-breadcrumb-item>
+            <a-breadcrumb-item><a href="">Application Center</a></a-breadcrumb-item>
+          </a-breadcrumb>
+        </div>
+        <!-- 用户信息 -->
+        <ul class="user-info">
+          <li>欢迎xiaoqiang<a-icon type="down" /></li>
+          <li>退出</li>
+        </ul>
+      </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    toggleCollapsed() {
+      this.$store.dispatch('changeCollapsed');
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/views/layout/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <!-- 菜单列表 -->
+    <MenuList />
+    <!-- 主要区域 -->
+    <div :class="{ 'main-app': true, 'menu-unflod': $store.state.collapsed }">
+      <MainList />
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import MenuList from '@/views/components/menu-list.vue';
+import MainList from '@/views/components/main-list.vue';
+
+export default {
+  components: {
+    MenuList,
+    MainList,
+  },
+  data() {
+    return {
+      collapsed: false,
+    };
+  },
+};
+</script>
+<style lang="less">
+@import url("~@/assets/CSS/home.less");
+@import url("~@/assets/CSS/reset.less");
+</style>
